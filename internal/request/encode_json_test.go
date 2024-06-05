a60cada6e113c11035b988fcbd2f27a2538f2d78
avnovoselov <avnovoselov@avito.ru>
2024-06-05 23:28:40 +0300

    add websocket server

--- internal/request/encode_json_test.go
+++ internal/request/encode_json_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/avnovoselov/live_debugger/internal/request"
 )
@@ -14,19 +15,17 @@ func TestEncodeJSON(t *testing.T) {
 
 	r1 := request.LogResponse{Offset: &expectedID}
 	j1, err := request.EncodeJSON(r1)
+	require.NoError(t, err)
 
 	r2 := request.LogResponse{}
 	j2, err := request.EncodeJSON(r2)
+	require.NoError(t, err)
 
 	r3 := request.LogResponse{Error: &expectedErr}
 	j3, err := request.EncodeJSON(r3)
+	require.NoError(t, err)
 
-	assert.NoError(t, err)
 	assert.Equal(t, `{"offset":123,"error":null}`, string(j1))
-
-	assert.NoError(t, err)
 	assert.Equal(t, `{"offset":null,"error":null}`, string(j2))
-
-	assert.NoError(t, err)
 	assert.Equal(t, `{"offset":null,"error":"error"}`, string(j3))
 }
